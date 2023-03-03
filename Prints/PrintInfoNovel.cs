4b4c19e807bab8e244602227c65a6f9343835611
алексей загородников <ayayv.ayayayv@yandex.ru>
2023-03-03 18:06:39 +0300

    PrintInfoNovel class created

---
+++ Prints/PrintInfoNovel.cs
@@ -0,0 +1,22 @@
+﻿using ParserFreedom.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParserFreedom.Prints
+{
+    public static class PrintInfoNovel
+    {
+        public static void PrintInfo(NovelModel novel)
+        {
+            Console.Clear();
+            Console.WriteLine($"{novel.Title}");
+            Console.WriteLine($"\nКоличество глав - {novel.CurentChapter}");
+            Console.WriteLine($"\n{novel.NovelFinished}");
+            Console.WriteLine($"\nПоследнее обновление - {novel.LatestAddition}");
+        }
+       
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using ParserFreedom.Configs;
 using ParserFreedom.Getters;
 using ParserFreedom.Models;
+using ParserFreedom.Prints;
 using System.Net;
 using System.Reflection;
 using System.Text;
@@ -18,24 +19,18 @@ using var handler = new HttpClientHandler
     Proxy = null,
     UseProxy = false
 };
-
-
 using var client = new HttpClient(handler);
 
 using var getterConfig = new HttpConfig(client, cookieContainer);
+var novelInfo = new FreedomInfoGetter(getterConfig);
 
 Console.WriteLine("Ведите название книги.");
 var novelName = "Сильнейшая Система Убийцы Драконов";
 
 try
 {
-    var novelInfo = new FreedomInfoGetter(getterConfig);
     var t = await novelInfo.Get(novelName);
-    Console.Clear();
-    Console.WriteLine($"{t.Title}");
-    Console.WriteLine($"\nКоличество глав - {t.CurentChapter}");
-    Console.WriteLine($"\n{t.NovelFinished}");
-    Console.WriteLine($"\nПоследнее обновление - {t.LatestAddition}");
+    PrintInfoNovel.PrintInfo(t);
 }
 catch (Exception ex)
 {
